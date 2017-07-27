c4b263fd9a2b74c0634ea3cc1f57bd3af0eb8dd3
Ashley.Beeson <ashley.beeson@qa.com>
2017-07-27 16:44:49 +0100

    First try at web app

--- src/model/FileReader.java
+++ src/model/FileReader.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/Practice")
+@WebServlet("/Reader")
 public class FileReader extends HttpServlet{
 
     private TestFileDto fileDto;
